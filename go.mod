9a3ec626f1a0c6c3d740ed6f62f9938ae6dc853d
wcx <56676157+juhaozero@users.noreply.github.com>
2023-07-14 11:33:27 +0800

    add 配置类，日志类和redis 工具类

---
+++ go.mod
@@ -0,0 +1,22 @@
+module util
+
+go 1.20
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	go.uber.org/zap v1.24.0
+)
+
+require github.com/google/uuid v1.1.1 // indirect
+
+require (
+	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/lestrrat-go/strftime v1.0.6 // indirect
+	github.com/micro/go-micro/v2 v2.9.1
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
---
+++ uredis/redis.go
@@ -0,0 +1,413 @@
+package uredis
+
+import (
+	"errors"
+
+	"strconv"
+	"time"
+
+	"log"
+
+	"github.com/go-redis/redis"
+
+	"go.uber.org/zap"
+)
+
+const (
+	Repeated_Times    = 5   //次数
+	Repeated_Interval = 200 //毫秒
+
+	Default_Time = time.Duration(-1) * time.Second
+)
+
+func TTL(con *redis.Client, key string) (time.Duration, error) {
+	return con.TTL(key).Result()
+}
+
+// 设置过期时间
+func Expire(con *redis.Client, key string, t int64) error {
+	return con.Expire(key, time.Duration(t)*time.Second).Err()
+}
+
+// 删除key
+func DelKey(con *redis.Client, key ...string) error {
+	return con.Del(key...).Err()
+}
+
+// ### string 类型
+
+// 获取string类型数据
+func GetString(con *redis.Client, key string) (string, error) {
+	count := 1
+	for {
+		result, err := con.Get(key).Result()
+		if err == nil {
+			return result, nil
+		}
+		if err == redis.Nil {
+			return "", nil
+		}
+		count++
+		if count > Repeated_Times {
+
+			log.Fatal("GetString 失败 ", zap.Any("key", key), zap.Error(err))
+			return "", err
+		} else {
+			time.Sleep(Repeated_Interval * time.Millisecond) //重试间隔
+		}
+	}
+}
+
+// 设置string类型数据
+func SetString(con *redis.Client, key string, value interface{}, ex ...int64) error {
+	count := 1
+	var t = Default_Time
+	if len(ex) > 0 {
+		t = time.Duration(ex[0]) * time.Second
+	}
+	for {
+		err := con.Set(key, value, t).Err()
+		if err == nil {
+			return nil
+		}
+		count++
+		if count > Repeated_Times {
+			log.Fatal("SetString 失败", zap.Any("key", key), zap.Error(err))
+			return err
+		} else {
+			time.Sleep(Repeated_Interval * time.Millisecond) //重试间隔
+		}
+	}
+}
+
+// ### zset 类型
+
+// 添加有序集合
+func ZSortSet(con *redis.Client, key string, value ...redis.Z) error {
+	if err := con.ZAdd(key, value...).Err(); err != nil {
+		log.Fatal("ZSortSet 失败 ", zap.Any("key", key), zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+// 设置对应分数
+func ZScore(con *redis.Client, key string, value string) error {
+	if err := con.ZScore(key, value).Err(); err != nil {
+		log.Fatal("ZScore 失败 ", zap.Any("key", key), zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+// 数量
+func ZCard(con *redis.Client, key string, value string) (int64, error) {
+	count, err := con.ZCard(key).Result()
+	if err != nil {
+		log.Fatal("ZScore 失败 ", zap.Any("key", key), zap.Error(err))
+		return count, err
+	}
+	return count, nil
+}
+
+// 获取升序集合数据 (从小到大, num[0]=start num[1]=end)
+func GetZRageSort(con *redis.Client, key string, num ...int64) ([]string, error) {
+	var n1, n2 int64 = 0, -1
+	if len(num) > 0 {
+		n1 = num[0]
+	}
+	if len(num) > 1 {
+		n2 = num[1]
+	}
+	arr, err := con.ZRange(key, n1, n2).Result()
+	if err != nil {
+		return nil, err
+	}
+	return arr, nil
+}
+
+// 获取降序集合数据  (从大到小,num[0]=start num[1]=end,默认全部)
+func GetZRevRangeSort(con *redis.Client, key string, num ...int64) ([]string, error) {
+	var n1, n2 int64 = 0, -1
+	if len(num) > 0 {
+		n1 = num[0]
+	}
+	if len(num) > 1 {
+		n2 = num[1]
+	}
+	arr, err := con.ZRevRange(key, n1, n2).Result()
+	if err != nil {
+		log.Fatal("GetZRevRangeSort 失败 ", zap.Any("key", key), zap.Error(err))
+		return nil, err
+	}
+	return arr, nil
+}
+
+// 获取score过滤后的升序集合数据  (从小到大)/min,max
+func GetZRevRangeByScoreSort(con *redis.Client, key string, fields ...string) ([]string, error) {
+
+	if len(fields) == 0 {
+		return nil, errors.New("参数错误")
+	}
+
+	arr, err := con.ZRangeByScore(key, redis.ZRangeBy{
+		Min: fields[0],
+		Max: fields[1],
+	}).Result()
+	if err != nil {
+		return nil, err
+	}
+	return arr, nil
+}
+
+// 获取score过滤后的降序集合数据  (从小到大,)/min,max
+func GetZRageRangeByScoreSort(con *redis.Client, key string, fields ...string) ([]string, error) {
+
+	if len(fields) == 0 {
+		return nil, errors.New("参数错误")
+	}
+	arr, err := con.ZRevRangeByScore(key, redis.ZRangeBy{
+		Min: fields[0],
+		Max: fields[1],
+	}).Result()
+	if err != nil {
+		return nil, err
+	}
+	return arr, nil
+}
+
+// 删除指定区间数据
+func DelZSortData(con *redis.Client, key string, num ...int64) error {
+	if len(num) == 0 {
+		return errors.New("参数错误")
+	}
+	s := strconv.FormatInt(num[0], 10)
+	e := strconv.FormatInt(num[1], 10)
+	err := con.ZRemRangeByLex(key, s, e).Err()
+	return err
+}
+
+// ### list 类型
+
+// 某个元素是否存在
+func HashExists(con *redis.Client, key string, field string) (bool, error) {
+	return con.HExists(key, field).Result()
+}
+
+// 压入链表
+func LPush(con *redis.Client, key string, value interface{}) (int64, error) {
+	return con.LPush(key, value).Result()
+}
+
+// 截断链表
+func LTrim(con *redis.Client, key string, num ...int64) error {
+	return con.LTrim(key, num[0], num[1]).Err()
+}
+
+// 弹出链表
+func LPop(con *redis.Client, key string) (string, error) {
+	return con.LPop(key).Result()
+}
+
+// 设置某个下标元素的值
+func LSet(con *redis.Client, key string, index int64, value interface{}) (string, error) {
+	return con.LSet(key, index, value).Result()
+}
+
+// 获取指定范围的链表
+func Lrange(con *redis.Client, key string, num ...int64) ([]string, error) {
+	var n1, n2 int64 = 0, -1
+	if len(num) > 0 {
+		n1 = num[0]
+	}
+	if len(num) > 1 {
+		n2 = num[1]
+	}
+
+	arr, err := con.LRange(key, n1, n2).Result()
+	if err != nil {
+		return nil, err
+	}
+	return arr, nil
+}
+
+// 根据值移除
+func LRem(con *redis.Client, key string, count int64, value interface{}) error {
+	return con.LRem(key, count, value).Err()
+}
+
+// 链表数量
+func LLen(con *redis.Client, key string) (int64, error) {
+	return con.LLen(key).Result()
+}
+
+// ### set 类型
+
+// 添加元素
+func SAdd(con *redis.Client, key string, value interface{}, ex ...int64) error {
+	if err := con.SAdd(key, value).Err(); err != nil {
+		return err
+	}
+	var t = Default_Time
+	if len(ex) > 0 {
+		t = time.Duration(ex[0]) * time.Second
+	}
+
+	if err := con.Expire(key, t).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 获取所有元素
+func SMembers(con *redis.Client, key string) ([]string, error) {
+	return con.SMembers(key).Result()
+
+}
+
+// 元素是否存在集合中
+func SIsMember(con *redis.Client, key string, field string) (bool, error) {
+	arr, err := con.SIsMember(key, field).Result()
+	return arr, err
+}
+
+// 删除元素
+func SRem(con *redis.Client, key string, field ...string) error {
+	return con.SRem(key, field).Err()
+}
+
+// 随机获取一个元素
+func SPop(con *redis.Client, key string, field ...string) (string, error) {
+	return con.SPop(key).Result()
+}
+
+// 数量
+func SCard(con *redis.Client, key string, field ...string) (int64, error) {
+	return con.SCard(key).Result()
+}
+
+// ### hash 类型
+
+// 获取Hash类型数据
+func GetHash(con *redis.Client, key string, field string) (string, error) {
+	count := 1
+	for {
+		result, err := con.HGet(key, field).Result()
+		if err == nil {
+			return result, nil
+		}
+		if err == redis.Nil {
+			return "", nil
+		}
+		count++
+		if count > Repeated_Times {
+			log.Fatal("GetHash 失败", zap.Any("key", key), zap.Error(err))
+			return "", err
+		} else {
+			time.Sleep(Repeated_Interval * time.Millisecond) //重试间隔
+		}
+	}
+}
+
+// 获取Hash全部数据数据
+func GetHashAll(con *redis.Client, key string) (map[string]string, error) {
+	count := 1
+	for {
+		result, err := con.HGetAll(key).Result()
+		if err == nil {
+			return result, nil
+		}
+		if err == redis.Nil {
+			return nil, nil
+		}
+		count++
+		if count > Repeated_Times {
+			log.Fatal("GetHashAll 失败 ", zap.Any("key", key), zap.Error(err))
+			return nil, err
+		} else {
+			time.Sleep(Repeated_Interval * time.Millisecond) //重试间隔
+		}
+	}
+}
+
+// 获取hash的长度
+func GetHashLen(con *redis.Client, key string) (int64, error) {
+	result, err := con.HLen(key).Result()
+	return result, err
+}
+
+// SetHash 设置Hash集合
+func SetHash(con *redis.Client, key, field string, value []byte) error {
+	count := 1
+	for {
+		err := con.HSet(key, field, value).Err()
+		if err == nil {
+			return nil
+		}
+		count++
+		if count > Repeated_Times {
+			log.Fatal("SetHash 失败 ", zap.Any("key", key), zap.Error(err))
+			return err
+		} else {
+			time.Sleep(Repeated_Interval * time.Millisecond) //重试间隔
+		}
+	}
+}
+
+// 删除hash
+func DelHash(con *redis.Client, key string, field ...string) error {
+	err := con.HDel(key, field...).Err()
+	return err
+}
+
+func SetNx(con *redis.Client, key string, value interface{}, ex ...int64) (bool, error) {
+	var t = Default_Time
+	if len(ex) > 0 {
+		t = time.Duration(ex[0]) * time.Second
+	}
+	return con.SetNX(key, value, t).Result()
+}
+
+func Keys(con *redis.Client, key string) ([]string, error) {
+	return con.Keys(key).Result()
+}
+
+func IsExists(con *redis.Client, key string) (bool, error) {
+	ENum, err := con.Exists(key).Result()
+	if err == nil {
+		if ENum > 0 {
+			return true, err
+		}
+	}
+	return false, err
+}
+
+func IncrKey(con *redis.Client, key string) (int64, error) {
+	i, err := con.Incr(key).Result()
+	if err != nil {
+		return 0, err
+	}
+	return i, err
+}
+func DecrKey(con *redis.Client, key string) (int64, error) {
+	i, err := con.Decr(key).Result()
+	if err != nil {
+		return -1, err
+	}
+	return i, err
+}
+
+func Incr(con *redis.Client, key string) error {
+
+	return con.Incr(key).Err()
+}
+
+func IncrBy(con *redis.Client, key string, val int64) (int64, error) {
+	return con.IncrBy(key, val).Result()
+}
+
+func Decr(con *redis.Client, key string) error {
+	return con.Decr(key).Err()
+
+}
