cb751d2612a3169da1e74d4771305b1f21fcaed2
juhaozero <56676157+juhaozero@users.noreply.github.com>
2026-06-10 10:30:53 +0800

    v0.1.0

--- common/common.go
+++ common/common.go
@@ -6,7 +6,6 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/juhaozero/util/model"
 
@@ -14,25 +13,6 @@ import (
 	json "github.com/json-iterator/go"
 	"github.com/shopspring/decimal"
 )
-
-func GetTimeMicro() int64 {
-	return time.Now().UnixMicro()
-}
-func GetTimeNano() int64 {
-	return time.Now().UnixNano()
-}
-func GetTimeMs() int64 {
-	return time.Now().UnixMilli()
-}
-func GetTime() int64 {
-	return time.Now().Unix()
-}
-func GetTimeFormat(format string) string {
-	return time.Now().Format(format)
-}
-func GetNumIsEven[T model.Number](data T) bool {
-	return int64(data)&1 == 0
-}
 
 // DoubleToString 小数类型转 string类型
 func DoubleToString[T model.Float](data T) string {
@@ -76,118 +56,6 @@ func GetKey(key string, separator string, field ...string) (s string) {
 		data.WriteString(v)
 	}
 	return data.String()
-}
-func GetDayTimeFormat[T model.Number](day T, format string) string {
-	return time.Now().AddDate(0, 0, int(day)).Format(format)
-}
-
-// GetTimeIsSame 判断时间是否是n天前/后
-// day 偏移的天数
-func GetTimeIsSame[T model.Number](times, day T, format string) bool {
-	now := time.Now().AddDate(0, 0, int(day)).Format(format)
-	sign := time.Unix(int64(times), 0).Format(format)
-	return now == sign
-}
-
-// GetExpDaySecond 获取n天前/后相差的秒数
-// day 偏移的天数
-func GetExpDaySecond[T model.Number](day T) T {
-	timeStr := time.Now().Format("2006-01-02")
-	t2, _ := time.ParseInLocation("2006-01-02", timeStr, time.Local)
-	if day > 0 {
-		return T(t2.AddDate(0, 0, int(day)).Unix() - time.Now().Unix())
-	} else {
-		return T(time.Now().Unix() - t2.AddDate(0, 0, int(day)).Unix())
-	}
-}
-
-// GetExpTime 获取偏移后的时间类型
-func GetExpTime[T model.Number](bufferTime T) time.Time {
-	return time.Now().Add(time.Duration(bufferTime))
-}
-
-// GetExpUnix 获取偏移后的时间戳
-func GetExpUnix(day int) int64 {
-	return time.Now().AddDate(0, 0, day).Unix()
-}
-
-// GetMonthDays 获取一个时间当月共有多少天
-func GetMonthDays(t time.Time) int {
-	t = GetToday(t)
-	year, month, _ := t.Date()
-	if month != 2 {
-		if month == 4 || month == 6 || month == 9 || month == 11 {
-			return 30
-		}
-		return 31
-	}
-
-	if ((year%4 == 0) && (year%100 != 0)) || year%400 == 0 {
-		return 29
-	}
-
-	return 28
-}
-
-// WeekDay 获取一个时间是星期几
-//   - 1 ~ 7
-func WeekDay(t time.Time) int {
-	t = GetToday(t)
-	week := int(t.Weekday())
-	if week == 0 {
-		week = 7
-	}
-
-	return week
-}
-
-// GetToday 获取一个时间的今天
-func GetToday(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
-}
-
-// GetSecond 获取共有多少秒
-func GetSecond(d time.Duration) int {
-	return int(d / time.Second)
-}
-
-// IsSameDay 两个时间是否是同一天
-func IsSameDay(t1, t2 time.Time) bool {
-	t1, t2 = GetToday(t1), GetToday(t2)
-	return t1.Unix() == t2.Unix()
-}
-
-// IsSameHour 两个时间是否是同一小时
-func IsSameHour(t1, t2 time.Time) bool {
-	return t1.Hour() == t2.Hour() && t1.Day() == t2.Day() && t1.Month() == t2.Month() && t1.Year() == t2.Year()
-}
-
-/*
-*
-获取指定周一的时间
-week:
-
-	-1:上周
-	 0:本周
-	 1:下周
-
-*
-*/
-func WeekIntervalTime(week int) (startTime string) {
-	now := time.Now()
-	offset := int(time.Monday - now.Weekday())
-	//周日做特殊判断 因为time.Monday = 0
-	if offset > 0 {
-		offset = -6
-	}
-
-	year, month, day := now.Date()
-	thisWeek := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-
-	startTime = thisWeek.AddDate(0, 0, offset+7*week).Format("2006-01-02")
-	//endTime = thisWeek.AddDate(0, 0, offset+6+7*week).Format("2006-01-02")
-
-	return startTime
 }
 
 // GetRandom 随机数区间
@@ -253,7 +121,7 @@ func CopyPoint(m any) any {
 	return newoby.Interface()
 }
 
-// 结构体转map
+// StructToMapString 结构体转map
 func StructToMapString(obj any) map[string]string {
 	mapping := make(map[string]string)
 	var valueOf = reflect.ValueOf(obj)
--- go.mod
+++ go.mod
@@ -22,7 +22,7 @@ require (
 )
 
 require (
-	github.com/coreos/bbolt v0.0.0-00010101000000-000000000000 // indirect
+	github.com/coreos/bbolt v1.3.2 // indirect
 	github.com/coreos/go-semver v0.3.1 // indirect
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20230601102743-20bbbf26f4d8 // indirect
@@ -33,9 +33,10 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.14.5 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
@@ -55,10 +56,7 @@ require (
 	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/time v0.3.0 // indirect
-	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230726155614-23370e0ffb3e // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230706204954-ccb25ca9f130 // indirect
-	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/genproto v0.0.0-20190927181202-20e1ac93f88c // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
@@ -83,7 +81,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.9.0
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
@@ -97,15 +95,9 @@ require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.27.8 // indirect
-	github.com/xuri/excelize/v2 v2.0.0-00010101000000-000000000000
+	github.com/xuri/excelize/v2 v2.6.0
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
+	google.golang.org/grpc v1.26.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
-
-replace (
-	github.com/360EntSecGroup-Skylar/excelize/v2 => github.com/xuri/excelize/v2 v2.6.0
-	github.com/coreos/bbolt => go.etcd.io/bbolt v1.3.4
-	github.com/xuri/excelize/v2 => github.com/360EntSecGroup-Skylar/excelize/v2 v2.6.0
-	google.golang.org/grpc => google.golang.org/grpc v1.26.0
 )
